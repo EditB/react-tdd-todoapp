8376bc02ca1495291b1b94c3b5cf92117151a2ce
EditB <beditto@yahoo.com>
2018-04-14 14:54:43 +1000

    Fixed up ESLint issues

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
+/* global it, expect */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+configure({ adapter: new Adapter() });
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
